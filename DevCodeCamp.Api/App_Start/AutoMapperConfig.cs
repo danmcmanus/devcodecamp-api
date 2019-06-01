f970c948af6e97fd2b815236fa755cc42628b398
danmcmanus <danmcmanus11@outlook.com>
2019-06-01 01:42:16 -0500

    added CRUD logic for instructors

---
+++ DevCodeCamp.Api/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using AutoMapper;
+
+namespace DevCodeCamp.Api.App_Start
+{
+    public static class AutoMapperConfig
+    {
+        public static void ConfigureMappings(UnityContainer container)
+        {
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.ConstructServicesUsing((type) =>
+                {
+                    return container.Resolve(type);
+
+                });
+            });
+        }
+    }
+}
---
+++ DevCodeCamp.Api/Controllers/InstructorsController.cs
@@ -0,0 +1,72 @@
+﻿using DevCodeCamp.Domain.Models;
+using System.Threading.Tasks;
+using System.Web.Http;
+using DCC.Domain.Services;
+
+namespace DevCodeCamp.Api.Controllers
+{
+    [RoutePrefix("instructors")]
+    public class InstructorsController : ApiController
+    {
+        private readonly IInstructorsService _instructorsService;
+
+        public InstructorsController(IInstructorsService instructorsService)
+        {
+            _instructorsService = instructorsService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> GetInstructors()
+        {
+            var response = await _instructorsService.GetAllInstructorsAsync();
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("{instructorId}")]
+        public async Task<IHttpActionResult> GetInstructorById(int instructorId)
+        {
+            var response = await _instructorsService.GetInstructorByIdAsync(instructorId);
+            return Ok(response);
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IHttpActionResult> AddInstructor([FromBody]InstructorRequest request)
+        {
+            var response = await _instructorsService.AddInstructorAsync(request);
+            if (response.IsError)
+            {
+                return BadRequest(response.ErrorMessage);
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPatch]
+        [Route("{instructorId}")]
+        public async Task<IHttpActionResult> UpdateInstructorData([FromBody]UpdateInstructorRequest request)
+        {
+            var response = await _instructorsService.UpdateInstructorAsync(request);
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("rate")]
+        public async Task<IHttpActionResult> RateInstructor([FromBody] RateInstructorRequest request)
+        {
+            var response = await _instructorsService.RateInstructorAsync(request);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("{instructorId}")]
+        public async Task<IHttpActionResult> Delete(int instructorId)
+        {
+            var response = await _instructorsService.DeleteInstructorAsync(instructorId);
+            return Ok(response);
+        }
+    }
+}
